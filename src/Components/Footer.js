eec480bad650fd984effaf5b6c7be45c5d08cbeb
yhsol <yhsol1592@gmail.com>
2019-05-09 19:19:41 +0900

    add Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 600;
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const ListItem = styled.li`
+  &:not(:last-child) {
+    margin-right: 20px;
+  }
+`;
+
+const Link = styled.a`
+  color: ${props => props.theme.darkBlueColor};
+`;
+
+const Copyright = styled.div`
+  color: ${props => props.theme.darkGreyColor};
+`;
+
+export default () => (
+  <Footer>
+    <List>
+      <ListItem>
+        <Link href="#">ABOUT US</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">SUPPORT</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PRESS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">API</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">JOBS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PRIVACY</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">TERMS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">DIRECTORY</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">PROFILES</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">HASHTAGS</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="#">LANGUAGE</Link>
+      </ListItem>
+      <Copyright>&copy; {new Date().getFullYear()} INSTACLONE</Copyright>
+    </List>
+  </Footer>
+);
