66c1725edfa46fbba033d8b870370a4394160763
yhsol <yhsol1592@gmail.com>
2019-05-09 19:19:28 +0900

    add Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "../Styles/theme";
+import RouterComponent from "./Router";
+import { useQuery } from "react-apollo-hooks";
+import { QUERY } from "./AppQuery";
+import Footer from "./Footer";
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
 
 export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+  // console.log(data);
+
   return (
-    <>
-      Hello!
-      <GlobalStyles />
-    </>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <RouterComponent isLoggedIn={isLoggedIn} />
+        <Footer />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
