0773fa1522d22b8000ee6816b936133bbbaa87de
yhsol <yhsol1592@gmail.com>
2019-05-09 19:43:14 +0900

    add Input

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Input from "../Components/Input";
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   min-height: 80vh;
+  flex-direction: column;
 `;
+
 const Box = styled.div`
   ${props => props.theme.whiteBox};
   border-radius: 0px;
-  width: 350px;
+  max-width: 350px;
+  width: 100%;
 `;
+
 const StateChanger = styled(Box)`
   text-align: center;
   padding: 20px 0px;
@@ -22,11 +27,21 @@ const Link = styled.span`
   cursor: pointer;
 `;
 
+const Form = styled(Box)`
+  padding: 38px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  width: 100%;
+`;
+
 export default () => {
   const [action, setAction] = useState("logIn");
 
   return (
     <Wrapper>
+      <Form>
+        <Input />
+      </Form>
       <StateChanger>
         {action === "logIn" ? (
           <>
